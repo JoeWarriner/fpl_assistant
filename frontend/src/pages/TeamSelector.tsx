44ea19e6e085ceb71448639f6e59e2e7c9f5aad7
Joe Warriner <joewarriner40@gmail.com>
2023-07-14 18:42:39 +0100

    Joined up backend and frontend

--- frontend/src/pages/TeamSelector.tsx
+++ frontend/src/pages/TeamSelector.tsx
@@ -1,8 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+const getTeamData = async () => {
+    try {
+        const response = await fetch("http://127.0.0.1:8000/best-team");
+        const team: string[] = await response.json() as string[];
+        return team;
+    } catch (error)  {
+        console.log('Error getting data from server: ', error)
+        return [];
+    }
+}
+
 const TeamSelector = () => {
+
+    const [team, setTeam] = useState<string[]>([]);
+    
+    useEffect(() => {
+        getTeamData().then((result) => {
+            setTeam(result);
+         }).catch((error) => {
+            console.log("Error fetching data", error)
+         });
+    }, []);
+
+    
     return (
         <>
-            <p>This is the team selector page.</p>
-            
+            <p>This is the team selector page. The best team is: </p>
+            {team.map((player) => <p> -  {player} </p> )}
         </>
     )
 };
