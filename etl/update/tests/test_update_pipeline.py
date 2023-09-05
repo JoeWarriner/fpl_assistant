c8167f733dd88cffbd9a6a60152b74af887cf30e
Joe Warriner <joewarriner40@gmail.com>
2023-09-05 15:03:11 +0100

    Added team season test

--- etl/update/tests/test_update_pipeline.py
+++ etl/update/tests/test_update_pipeline.py
@@ -98,10 +98,13 @@ def insert_season(database):
 def import_players(insert_season):
     orchestrator = PipelineOrchestrator()
     orchestrator.add_task(players)
-    orchestrator.run()
+    return orchestrator
+
+
 
 
 def test_player_import(import_players):
+    import_players.run()
     players = db.dal.session.execute(select(
         db.Player)
     ).all()
@@ -116,23 +119,87 @@ def test_player_import(import_players):
     assert alvarez.fpl_id == 461358
 
 
+@pytest.fixture
+def import_teams(import_players):
+    orchestrator = import_players
+    orchestrator.add_task(teams)
+    return orchestrator
+
+def test_team_import(import_teams):
+    import_teams.run()
+    teams = db.dal.session.execute(select(
+        db.Team)
+    ).all()
+    assert len(teams) == 4
+
+    liverpool, = db.dal.session.execute(select(db.Team).where(db.Team.short_name == 'LIV')).one()
+    assert liverpool.team_name == 'Liverpool'
+    assert liverpool.fpl_id == 14
+
+    city, = db.dal.session.execute(select(db.Team).where(db.Team.short_name == 'MCI')).one()
+    assert city.team_name == 'Man City'
+    assert city.fpl_id == 43
 
 
+@pytest.fixture
+def import_positions(import_teams):
+    orchestrator = import_teams
+    orchestrator.add_task(positions)
+    return orchestrator
 
-# def test_team_import(database):
-#     orchestrator = PipelineOrchestrator()
-#     test_add_team = DataImportPipeline(
-#         extracter= extracters.APIExtracter(api.Team, test_data.ARSENAL),
-#         transformer= adapters.APITranformer(adapter=adapters.TeamAdapter),
-#         loader = loaders.DBLoader(db.Team)
-#     )
-#     orchestrator.add_task(test_add_team)
-#     orchestrator.run()
+def test_position_import(import_positions):
+    import_positions.run()
+    positions = db.dal.session.execute(select(
+        db.Position)
+    ).all()
+    assert len(positions) == 4
 
-#     team = db.dal.session.scalar(select(db.Team))
-#     assert team.team_name == 'Arsenal'
-#     assert team.short_name == 'ARS'
-#     assert team.fpl_id == 3
+    goalkeeper, = db.dal.session.execute(select(db.Position).where(db.Position.short_name == 'GKP')).one()
+    assert goalkeeper.fpl_id == 1
+    assert goalkeeper.pos_name == 'Goalkeeper'
+
+    forward, = db.dal.session.execute(select(db.Position).where(db.Position.short_name == 'FWD')).one()
+    assert forward.pos_name == 'Forward'
+    assert forward.fpl_id == 4
+
+
+@pytest.fixture
+def import_team_seasons(import_positions):
+    orchestrator = import_positions
+    orchestrator.add_task(team_seasons, predecessors={teams})
+    return orchestrator
+
+
+def team_season_test_query(fpl_id, season):
+    output, = db.dal.session.execute(
+        select(
+            db.Team.short_name
+        ).select_from(
+            db.TeamSeason
+        ).join(
+            db.Team, db.Team.id == db.TeamSeason.team_id
+        ).join(
+            db.Season, db.Season.id == db.TeamSeason.season_id
+        ).where(
+            db.TeamSeason.fpl_id == fpl_id , db.Season.season == season
+        )
+    ).one()
+    return output
+
+def test_team_season_import(import_team_seasons):
+    import_team_seasons.run()
+    team_seasons = db.dal.session.execute(select(
+        db.TeamSeason)
+    ).all()
+    assert len(team_seasons) == 4
+
+    liverpool_2023_short_name = team_season_test_query(11, '2023-24')
+    assert liverpool_2023_short_name == 'LIV'
+    
+    city_2023_short_name = team_season_test_query(13, '2023-24')
+    assert city_2023_short_name == 'MCI'
+
+
 
 
 
