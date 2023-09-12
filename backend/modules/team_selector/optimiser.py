82ece8521980e4ff555d8711ce9ddd99482fa13b
Joe Warriner <joewarriner40@gmail.com>
2023-09-12 19:00:36 +0100

    Added working optimiser

--- backend/modules/team_selector/optimiser.py
+++ backend/modules/team_selector/optimiser.py
@@ -39,28 +39,32 @@ def get_player_data() -> pd.DataFrame:
 def get_optimised_team(player_data: pd.DataFrame) -> list[str]:
 
     ## Extract relevant variables
-    players = player_data['full_name']
-    teams = player_data['team']
+    players = player_data['id']
+    teams = player_data['team_id']
+
     distinct_teams = teams.unique()
-    total_points = player_data['total_points']
-    cost = player_data['now_cost']
-    is_gk = player_data['element_type'] == 'GK'
-    is_mid = player_data['element_type'] == 'MID' 
-    is_def = player_data['element_type'] == 'DEF'
-    is_fwd = player_data['element_type'] == 'FWD'
+
+    predicted_score = player_data['predicted_score']
+    cost = player_data['current_value']
+
+    is_gk = player_data['position_id'] == 1
+    is_mid = player_data['position_id'] == 3
+    is_def = player_data['position_id'] == 2
+    is_fwd = player_data['position_id'] == 4
 
     model = pulp.LpProblem("Integer_Programming_Problem", pulp.LpMaximize)
 
     ## Objective
     x = pulp.LpVariable.dicts('x', players, cat='Binary')
-    model += pulp.lpSum([total_points[i] * x[player] for i, player in enumerate(players)])
+    model += pulp.lpSum([predicted_score[i] * x[player] for i, player in enumerate(players)])
 
     ## Constraints
-    model += pulp.lpSum([is_gk[i] * x[player] for i, player in enumerate(players)]) == 1
-    model += pulp.lpSum([is_mid[i] * x[player] for i, player in enumerate(players)]) == 4
-    model += pulp.lpSum([is_def[i] * x[player] for i, player in enumerate(players)]) == 4
-    model += pulp.lpSum([is_fwd[i] * x[player] for i, player in enumerate(players)]) == 2
+    model += pulp.lpSum([is_gk[i] * x[player] for i, player in enumerate(players)]) == 2
+    model += pulp.lpSum([is_mid[i] * x[player] for i, player in enumerate(players)]) == 5
+    model += pulp.lpSum([is_def[i] * x[player] for i, player in enumerate(players)]) == 5
+    model += pulp.lpSum([is_fwd[i] * x[player] for i, player in enumerate(players)]) == 3
     model += pulp.lpSum([cost[i] * x[player] for i, player in enumerate(players)]) <= 1000
+
     for team in distinct_teams:
         model += pulp.lpSum([(teams[i] == team) * x[player] for i, player in enumerate(players)]) <= 3
 
@@ -68,3 +72,4 @@ def get_optimised_team(player_data: pd.DataFrame) -> list[str]:
     model.solve()
     selected_players  = [player for player in players if x[player].value() == 1]
     return(selected_players)
+
--- backend/modules/team_selector/tests/test_optimiser.py
+++ backend/modules/team_selector/tests/test_optimiser.py
@@ -1,17 +1,107 @@
+import pytest
 import modules.team_selector.optimiser as optimiser
 from database.test_utils import populated_database_with_predictions
 
 import pandas as pd
 
-
+COLUMNS = ['id', 'current_value', 'predicted_score', 'position_id','team_id']
 
 def test_get_player_data(populated_database_with_predictions):
     output = optimiser.get_player_data()
     expected_output = pd.DataFrame(
-        columns = ['id', 'current_value', 'predicted_score', 'position_id','team_id'],
+        columns = COLUMNS,
         data = [[
             2, 55, 2.7, 1, 2
         ]]
     )
     pd.testing.assert_frame_equal(output, expected_output)
 
+
+@pytest.fixture
+def generic_team() -> list[list[int]]:
+    '''Get a list of players that forms a legal team'''
+    return [
+        # id, value, predicted_score, position_id, team id
+        [1, 65, 5, 1, 1],
+        [2, 65, 5, 1, 1],
+        [3, 65, 5, 2, 1],
+        [4, 65, 5, 2, 2],
+        [5, 65, 5, 2, 2],
+        [6, 65, 5, 2, 2],
+        [7, 65, 5, 2, 3],
+        [8, 65, 5, 3, 3],
+        [9, 65, 5, 3, 3],
+        [10, 65, 5, 3, 4],
+        [11, 65, 5, 3, 4],
+        [12, 65, 5, 3, 4],
+        [13, 65, 5, 4, 5],
+        [14, 65, 5, 4, 5],
+        [15, 65, 5, 4, 5],
+    ] 
+
+
+def test_optimised_team__basic_case(generic_team : list[list[int]]):
+    data = pd.DataFrame(columns = COLUMNS, data = generic_team)
+    output = optimiser.get_optimised_team(data)
+    assert len(output) == 15
+
+
+def test_optimised_team__poor_value_player(generic_team: list[list[int]]):
+    # Change defender with ID #7 to have low predicted points
+    generic_team[6][2] = 1 
+    # Add a better value defender at the end
+    generic_team.append(
+        [16, 65, 5, 2, 3]
+    ) 
+
+    data = pd.DataFrame(columns = COLUMNS, data = generic_team)
+    output = optimiser.get_optimised_team(data)
+    assert 7 not in output
+    assert 16 in output
+
+def test_optimised_team__high_value_player(generic_team: list[list[int]]):
+    # Add a MUCH better value defender at the end
+    generic_team.append(
+        [16, 65, 100, 2, 3]
+    ) 
+    data = pd.DataFrame(columns = COLUMNS, data = generic_team)
+    output = optimiser.get_optimised_team(data)
+    assert 16 in output
+
+    
+def test_optimised_team__too_expensive_player(generic_team: list[list[int]]):
+    # Add a MUCH better value defender at the end, who is too expensive
+    generic_team.append(
+        [16, 300, 100, 2, 3]
+    ) 
+    data = pd.DataFrame(columns = COLUMNS, data = generic_team)
+    output = optimiser.get_optimised_team(data)
+    assert 16 not in output
+
+
+def test_optimised_team__too_many_same_team_players(generic_team: list[list[int]]):
+    # Add a series of MUCH better value players, but who are on the same team.
+    generic_team.extend([
+        [16, 1, 100, 1, 3],
+        [17, 1, 100, 2, 3],
+        [18, 1, 100, 3, 3],
+        [19, 1, 90, 4, 3] # should be excluded.
+    ]) 
+    data = pd.DataFrame(columns = COLUMNS, data = generic_team)
+    output = optimiser.get_optimised_team(data)
+    assert 19 not in output
+
+
+def test_optimised_team__too_many_same_position_players(generic_team: list[list[int]]):
+    # Add a series of MUCH better value players, but who are all the same position
+    generic_team.extend([
+        [16, 1, 100, 4, 6],
+        [17, 1, 100, 4, 7],
+        [18, 1, 100, 4, 8],
+        [19, 1, 90, 4, 9] # should be excluded.
+    ]) 
+    data = pd.DataFrame(columns = COLUMNS, data = generic_team)
+    output = optimiser.get_optimised_team(data)
+    assert 19 not in output
+
+    
